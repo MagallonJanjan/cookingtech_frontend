b45a216328c0e6a7d175047ce05cb8f9a3d2e812
AnkleBreaker <MagallonJanjan@gmail.com>
2021-01-20 13:04:44 +0800

    Finish Validating Input

--- cookingtech/src/app/Components/Auth/register/register.component.ts
+++ cookingtech/src/app/Components/Auth/register/register.component.ts
@@ -10,28 +10,54 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 })
 export class RegisterComponent implements OnInit {
 
-  user = new FormGroup({
-    firstname : new FormControl('',[Validators.required, Validators.minLength(4)]),
-    lastname : new FormControl('',[Validators.required, Validators.minLength(4)]),
-    username : new FormControl('',[Validators.required, Validators.minLength(4)]),
-    email : new FormControl('',[Validators.required, Validators.email]),
-    usertype : new FormControl('',Validators.required),
-    password : new FormControl('',[Validators.required, Validators.minLength(8)])
-  })
+   
+user: any = FormGroup;
+submitted = false;
 
-  constructor() { }
+constructor(private formBuilder: FormBuilder) { }
 
+ngOnInit(): void {
+  this.user = this.formBuilder.group({
+    firstname: ['', [Validators.required, Validators.minLength(4)]],
+    lastname: ['', [Validators.required, Validators.minLength(4)]],
+    username: ['',[Validators.required, Validators.minLength(4)]],
+    email: ['', [Validators.required, Validators.email]],
+    usertype: ['',Validators.required],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmpassword: ['', Validators.required]
+  }, {
+    validator: this.MustMatch('password', 'confirmpassword')
+  });
+}
+get f() { return this.user.controls; }
 
-  ngOnInit(): void {
-    console.log('Init');
-    
+MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+
+    if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+      // return if another validator has already found an error on the matchingControl
+      return;
+    }
+    // set error on matchingControl if validation fails
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  }
+}
+onSubmit() {
+  this.submitted = true;
+  console.log('Hi')
+  // stop here if form is invalid
+  if (this.user.invalid) {
+    return;
   }
 
-  onSubmit(){
-    console.log(JSON.stringify(this.user.value))
-  }
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.user.value))
+}
 
-
-  
 
 }

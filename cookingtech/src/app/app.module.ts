d23a90766191f296afe41c4f42aa6ef2b9057c6a
2ndyrGroupC <2ndyrGroupC@pn.local>
2021-01-22 13:48:39 +0800

    updates in the admin dasboard

--- cookingtech/src/app/app.module.ts
+++ cookingtech/src/app/app.module.ts
@@ -9,14 +9,18 @@ import { HttpClientModule } from '@angular/common/http';
 //Import Components
 import { AppComponent } from './app.component';
 import { AdminComponent } from './Components/Public/admin/admin.component';
-
-
+import { AdminHeaderComponent } from './Components/admin-header/admin-header.component';
+import { AdminSidebarComponent } from './Components/admin-sidebar/admin-sidebar.component';
+import { AdminTableComponent } from './Components/admin-table/admin-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    AdminComponent
+    AdminComponent,
+    AdminHeaderComponent,
+    AdminSidebarComponent,
+    AdminTableComponent
   ],
   imports: [
     BrowserModule,

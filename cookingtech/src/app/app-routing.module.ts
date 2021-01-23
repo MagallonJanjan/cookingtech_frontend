c6de416056c1a85e61d2e41186bdd00ffd09aaf2
Serenity Violet <xylorose@gmail.com>
2021-01-23 10:47:26 +0800

    gojo

--- cookingtech/src/app/app-routing.module.ts
+++ cookingtech/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 //Auth Components
 import {LoginComponent} from '../app/Components/Auth/login/login.component'
 import {RegisterComponent} from '../app/Components/Auth/register/register.component'
+import { HomeComponent } from './Components/Public/home/home.component';
 
 const routes: Routes =
 [
   { path : 'login', component : LoginComponent },
-  { path : 'register', component : RegisterComponent }
+  { path : 'register', component : RegisterComponent },
+  { path : '', component : HomeComponent }
 ];
 
 @NgModule({

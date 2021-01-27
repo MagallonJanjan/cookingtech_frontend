0132d534cff134e2ea4159688c33557b632ae8dd
aparece1241 <apareceh7@gmail.com>
2021-01-28 00:08:28 +0800

    updates

--- src/app/Components/Public/addrecipe/addrecipe.component.ts
+++ src/app/Components/Public/addrecipe/addrecipe.component.ts
@@ -25,6 +25,7 @@ export class AddrecipeComponent implements OnInit {
 
   addRecipe: any;
   recipeId: any;
+  recipe: any;
   ngOnInit(): void {
 
     this.addRecipe = this.formBuilder.group({
@@ -39,9 +40,27 @@ export class AddrecipeComponent implements OnInit {
     })
 
 
-    this.route.paramMap.subscribe( params => {
-        this.recipeId = params.get('id');
-      });
+    this.route.paramMap.subscribe(params => {
+      this.recipeId = params.get('id');
+    });
+
+
+    if (this.recipeId) {
+      this.apiRequest.apiRequest(`/recipes/${this.recipeId}`,"get")
+        .subscribe( (respond:any)=> {
+          this.recipe = respond.recipe[0];
+          this.addRecipe = this.formBuilder.group({
+            name: [this.recipe.name, [Validators.required, Validators.minLength(6)]],
+            description: [this.recipe.description, [Validators.required, Validators.minLength(25)]],
+            yield: ['', Validators.required],
+            category: ['', Validators.required],
+            ingredients: ['', [Validators.required, Validators.minLength(3)]],
+            procedures: ['', [Validators.required, Validators.minLength(4)]],
+            tag: ['', [Validators.required, Validators.minLength(5)]],
+            img_url: ['', [Validators.required]]
+          })
+        } );
+    }
   }
 
   ingredientsArray: any[] = [];
@@ -51,9 +70,9 @@ export class AddrecipeComponent implements OnInit {
   addIngredients() {
     this.ingredientsArray.push(this.addRecipe.value.ingredients);
     (<HTMLInputElement>document.getElementById('lingling')).value = ""
-    
+
   }
- 
+
   addProcedure() {
     this.proceduresArray.push(this.addRecipe.value.procedures);
     (<HTMLInputElement>document.getElementById('procedure')).value = "";
@@ -71,8 +90,8 @@ export class AddrecipeComponent implements OnInit {
   }
 
 
-  datas:any;
-  onSubmit(data : any){ 
+  datas: any;
+  onSubmit(data: any) {
     this.addRecipe.value.ingredients = this.ingredientsArray;
     this.addRecipe.value.procedures = this.proceduresArray;
     this.datas = this.addRecipe.value;
@@ -80,10 +99,10 @@ export class AddrecipeComponent implements OnInit {
     let UserData = this.dataEnc.decrypt(cookie);
     this.datas["user_id"] = UserData.user.id;
 
-  
-    
-    this.apiRequest.apiRequest('/recipes','post', this.datas)
-      .subscribe( respond => {
+
+
+    this.apiRequest.apiRequest('/recipes', 'post', this.datas)
+      .subscribe(respond => {
         console.log(respond);
         alert('You are adding a new recipe!');
         this.addRecipe.reset()
@@ -95,14 +114,14 @@ export class AddrecipeComponent implements OnInit {
       })
   }
   disableAddButton = true;
-  procedures:any;
-  
+  procedures: any;
+
 
   // inputIngredients = (<HTMLInputElement>document.getElementById("lingling"));
   // inputProcedure =  (<HTMLInputElement>document.getElementById("procedure"));
 
-  
-   
-  
+
+
+
 
 }

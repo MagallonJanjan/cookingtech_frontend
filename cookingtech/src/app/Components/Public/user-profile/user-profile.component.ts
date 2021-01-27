0750068f8424b7589e33a0378bb8e4d35ab4c107
aparece1241 <apareceh7@gmail.com>
2021-01-26 21:44:39 -0800

    updates

--- cookingtech/src/app/Components/Public/user-profile/user-profile.component.ts
+++ cookingtech/src/app/Components/Public/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiRequestService } from '../../../services/apirequest.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private apiService : ApiRequestService
+  ) { }
+
+  user_id: any;
+  user: any;
   showSideBar:boolean = true;
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      params => {
+        this.user_id = params.get('id');
+      }
+    );
+      this.apiService.apiRequest(`/users/${this.user_id}`, "get")
+        .subscribe(
+          respond => {
+            this.user = respond;
+            console.log(this.user);
+          }
+        );
   }
 
   sideBarToggle(data:any) {

5858d9679b1d3b21f7c3f96c272ebe35a873c7fe
2ndyrGroupB <2ndyrGroupB@pn.local>
2021-01-28 21:14:46 +0800

    search bar

--- cookingtech/src/app/Components/Public/viewpage-header/viewpage-header.component.ts
+++ cookingtech/src/app/Components/Public/viewpage-header/viewpage-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiRequestService} from '../../../services/apirequest.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-viewpage-header',
@@ -7,11 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewpageHeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private apiService : ApiRequestService
+  ) { }
+  
+  recipes: any;
   cookie: any;
   ngOnInit(): void {
     this.cookie = window.localStorage.getItem('token');
+
+    this.apiService.apiRequest('/recipes', "get")
+        .subscribe((respond:any)=>{
+          this.recipes = respond;
+          console.log(this.recipes);
+         
+        })
+
+
+  }
+  values:any;
+  name:any;
+
+  onKey(item:any) {
+     
+        item = item.toLowerCase();
+        this.values = this.recipes.filter((recipe:any)=> {
+          return recipe.name.toLowerCase().includes(item);
+        });
+        console.log(this.values); 
+          
   }
 
 }

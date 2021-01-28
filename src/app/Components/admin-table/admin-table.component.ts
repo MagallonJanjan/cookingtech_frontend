49b7c625db4c029c24e03380a5389408c3b59b6a
aparece1241 <apareceh7@gmail.com>
2021-01-29 05:36:27 +0800

    updates

--- src/app/Components/admin-table/admin-table.component.ts
+++ src/app/Components/admin-table/admin-table.component.ts
@@ -150,4 +150,33 @@ export class AdminTableComponent implements OnInit {
   }
 
 
+   filterTable(){
+   var input, filter, table, tr, td, i, txtValue;
+    input = (<HTMLInputElement>document.getElementById('myInput'));
+    filter = input.value.toUpperCase();
+    table = (<HTMLTableElement>document.getElementById('adminTable'))
+    tr = table.getElementsByTagName("tr");
+   for (i = 0; i < tr.length; i++) {
+   td = tr[i].getElementsByTagName("td")[0];
+       if (td) {
+         txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+         } else {
+          tr[i].style.display = "none";
+        }
+       }       
+     }
+ }
+
+ user:any
+ resetPassword(){
+   this.user = this.user.value.id;
+   console.log(this.user);
+
+  // this.apiService.apiRequest(`/users/${this.user}`,{"password":})
+  //     .subscribe((respond:any)=>{
+
+  //     })   
+  }
 }
